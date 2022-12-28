2c2b8a38ce576fe664c7b86160e881330406c2a6
rehaaam33 <121580818+rehaaam33@users.noreply.github.com>
2022-12-28 23:00:15 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+namespace PerfectNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter The Begning : ");
+
+             int begning = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("please enter the End: ");
+
+             int end = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("The Perfect numbers within the given range : ");
+
+            for ( int n = begning; n <= end; n++)
+            {
+                int i = 1;
+                int sum = 0;
+                while (i < n)
+                {
+                    if (n % i == 0)
+                        sum = sum + i;
+                    i++;
+                }
+                if (sum == n)
+                    Console.WriteLine("{0} ", n);
+            }
+            Console.WriteLine("  ");
+        }
+    }
+}
